a6bceeafe99821d8a0a27d8c54e3610cc04bb482
Ryan Longacre <longacreryan@gmail.com>
2026-08-09 19:16:03 -0700

    made the modal slightly interactive, passes in the dom element that contains all the info (at least lecture for now, not sure about section

--- content_scripts.js
+++ content_scripts.js
@@ -10,13 +10,15 @@
     document.getElementsByClassName("wk-schedule js-full")[0].appendChild(document.importNode(classes, true));
 
     /*
-    note: the id is 52795T930, and T is the day of the week its on, and 930 is the start time. don't know what 52979 is
+    note: the id is 52795T930, and T is the day of the week its on, and 930 is the start time. don't know what 52979 is: its the like enroll code or smth on the thing
     note: also, for the data- things, syntax is object.dataset.day = 'x'; generates data-day = 'x';
     note: there is a ul in the original li, the id is pageContent_eventsGroupM or T or W or R or F depending on day of the week, so will have to be dynamic
     setup of the event in the calendar
     note: the position of the thing is entirely determined by the style: top: 0px part. 
     note: I think the position value is entirely dependent on the time from 8 oclock in minutes. so i just need to get that. 
     note: the id is probably just an identifier to add/remove each one which will be useful for me
+    note: class for the lecture part: sectionSelect, class for that ones sections is sections. both of these are under scheduleItem
+    note: the scheduleItem div is the event.parentNode.parentNode.parentNode but thats not what im clicking on
     <li class="single-event" id="something" data-day="T" data-start="9:30 AM" 
         data-end="10:45 AM" data-content="CMPSC   134 " data-event="ILP 3314" 
         style="top: 90px; height: 75px
@@ -28,40 +30,56 @@
         </a>
     </li>
     */
-   const sampleEvent = document.createElement("li");
 
-   sampleEvent.className = "single-event";
-   sampleEvent.dataset.day = "T";
-   sampleEvent.dataset.start = "01:00 PM";
-   sampleEvent.dataset.end = "01:45 PM";
-   sampleEvent.dataset.content = "CMPSC 156";
-   sampleEvent.dataset.event = "ILP 2201";
-   sampleEvent.style= "top: " + getSeparation("1:00 PM") + "px; height: 75px;";
 
-   const innerElement = document.createElement("a");
-   
-   const innerTitle = document.createElement("h1");
-   const innerLocation = document.createElement("h2");
-   const innerTime = document.createElement("p");
+    const modal = document.querySelector(".course-select-modal");
+    modal.addEventListener("click", (event) => {
+        console.log(event.target.outerHTML);
+        if (event.target.tagName != "DIV") {
+            console.log("Clicked the button");
+        }
+        else if (event.target.childElementCount === 1) {
+            console.log("Wrong click");
+            addNewEvent(event.target.parentNode);
+        } else {
+            console.log("Correct click");
+            addNewEvent(event.target);
+        }
+    });
 
-   innerTitle.className = "event-name";
-   innerTitle.innerText = "CMPSC 156";
-   
-   innerLocation.className = "event-location";
-   innerLocation.innerText = "ILP 2201";
-   
-   innerTime.innerText = "1:00 PM - 1:45PM";
+   const addNewEvent = (parentDiv) => {
+        const sampleEvent = document.createElement("li");
+        sampleEvent.className = "single-event";
+        sampleEvent.dataset.day = "T";
+        sampleEvent.dataset.start = "01:00 PM";
+        sampleEvent.dataset.end = "01:45 PM";
+        sampleEvent.dataset.content = "CMPSC 156";
+        sampleEvent.dataset.event = "ILP 2201";
+        sampleEvent.style= "top: " + getSeparation("1:00 PM") + "px; height: 75px;";
 
-   innerElement.appendChild(innerTitle);
-   innerElement.appendChild(innerLocation);
-   innerElement.appendChild(innerTime);
+        const innerElement = document.createElement("a");
+        
+        const innerTitle = document.createElement("h1");
+        const innerLocation = document.createElement("h2");
+        const innerTime = document.createElement("p");
 
-   sampleEvent.appendChild(innerElement);
+        innerTitle.className = "event-name";
+        innerTitle.innerText = "CMPSC 156";
+        
+        innerLocation.className = "event-location";
+        innerLocation.innerText = "ILP 2201";
+        
+        innerTime.innerText = "1:00 PM - 1:45PM";
 
-   targetCol = document.querySelector("#pageContent_eventsgroupT");
-   targetCol.querySelector(".single-event-ul").appendChild(sampleEvent);
-   
-   console.log(sampleEvent.innerHTML);
+        innerElement.appendChild(innerTitle);
+        innerElement.appendChild(innerLocation);
+        innerElement.appendChild(innerTime);
+
+        sampleEvent.appendChild(innerElement);
+
+        targetCol = document.querySelector("#pageContent_eventsgroupT");
+        targetCol.querySelector(".single-event-ul").appendChild(sampleEvent);
+   }
 
 
 
@@ -76,7 +94,6 @@
 
 //Want to make it so that you put in the string "9:30 AM", it outputs 90, cause thats 90 minutes away from 8
 function getSeparation(time) {
-    //should be just a august 9th at 8
     const [val, ap] = time.split(" ");
     const [hour, minute] = val.split(":").map(num => parseInt(num, 10));
     const adjustedHour = ap === "AM" ? hour : hour + 12;
