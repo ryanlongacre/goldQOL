c2df359de32981da05a415e228ad54bfb7247484
Ryan Longacre <longacreryan@gmail.com>
2026-08-23 00:16:32 -0700

    finally finished the basics of the collision detection. Now fully detecs overlaps, stores them in an Map (array => Map) where key is the class, and the value is a list of classes it overlaps with. Going to make a new branch for the actual decoration of what to do when a collision happens.

--- content_scripts.js
+++ content_scripts.js
@@ -65,10 +65,6 @@
 
         const up = await chrome.storage.local.get('times');
         const up2 = up.times || [];
-        console.log("New list");
-        for (const  li of up2) {
-            console.log(li);
-        }
         
     });
 
@@ -91,57 +87,35 @@
         const [title, code, days, time, location] = getInfo(parentDiv);
         const [start, end] = time.split("-").map(num => getSeparation(num));
 
-        const { r } = await chrome.storage.local.get('overlaps');
-        const currentOverlaps = new Map(r);
-        for (const [id, time] of currentTimes) {
-            const s = time.split("-")[0];
-            const [dayI, s2] = s.split("~");
-            const startI = getSeparation(s2);
-            const endI = getSeparation(time.split("-")[1]);
-            
-            for (const day of days.split("-")) {
-                const idOfElem = code + day + time.split("~")[1].split("-")[0].split(" ")[0].split(":").join("");
-                if (day !== dayI) {
-                    continue;
-                }
-                if ((start > startI) && (start < endI)) {
-                    console.log("Scenario 1");
-                    currentOverlaps.set(id, [...currentOverlaps.get(id) ?? [], idOfElem])
-                    currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], id])
-                } else if ((end > startI) && (end < endI)) {
-                    console.log("Scenario 2");
-                    currentOverlaps.set(id, [...currentOverlaps.get(id) ?? [], idOfElem])
-                    currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], id])
-                } else if ((start < startI) && (end > endI)) {
-                    console.log("Scenario 3");
-                    currentOverlaps.set(id, [...currentOverlaps.get(id) ?? [], idOfElem])
-                    currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], id])
-                }
-            }
-                
-        }
-
-        console.log("Overlaps: ");
-        console.log(currentOverlaps);
-
-        await chrome.storage.local.set({ overlaps: [...currentOverlaps]})
-
         
 
+        
+        //remove if already clicked
         if (currentList.includes(code)) {
             //Code to remove is code, day, then time
             let updatedTimes = [...currentTimes];
+
+            const { overlaps } = await chrome.storage.local.get('overlaps');
+            const currentOverlaps = new Map(overlaps);
             for (const day of days.split(" ")) {
                 const idOfElem = code + day + time.split("-")[0].split(" ")[0].split(":").join(""); 
-                        
                 const elementToRemove = document.getElementById(idOfElem);
                 elementToRemove.parentNode.removeChild(elementToRemove);
                 updatedTimes = updatedTimes.filter(item => item[0] !== idOfElem);
-                console.log("Updated times:");
-                for (const li of  updatedTimes) {
-                    console.log(li);
+
+                const mainOverlaps = currentOverlaps.get(idOfElem) || [];
+                console.log(mainOverlaps);
+                for (const item of mainOverlaps) {
+                    currentOverlaps.set(item, [...currentOverlaps.get(item).filter(item => item != idOfElem)]);
                 }
+                currentOverlaps.delete(idOfElem);
+
                 
+            }
+            for ([key, value] of currentOverlaps) {
+                if (value.length === 0) {
+                    currentOverlaps.delete(key);
+                }
             }
             await chrome.storage.local.set({times: updatedTimes});
             
@@ -149,8 +123,14 @@
             await chrome.storage.local.set({ current: updatedList });
 
                 
-        } else {
+            console.log("Overlaps: ");
+            console.log(currentOverlaps);
 
+            await chrome.storage.local.set({ overlaps: [...currentOverlaps]});  
+
+                
+        } else {
+            //not currently there so add it
             currentList.push(code);
             
             await chrome.storage.local.set({current: currentList});
@@ -162,7 +142,50 @@
                 targetCol.querySelector(".single-event-ul").appendChild(getNewElement(code, day, time, location, title));
             }
             await chrome.storage.local.set({times: currentTimes});    
+            const { overlaps } = await chrome.storage.local.get('overlaps');
+            const currentOverlaps = new Map(overlaps);
+            for (const [idCurr, timeCurr] of currentTimes) {
+                const s = timeCurr.split("-")[0];
+                const [dayI, s2] = s.split("~");
+                const startI = getSeparation(s2);
+                const endI = getSeparation(timeCurr.split("-")[1]);
+                
+                for (const day of days.split(" ")) {
+                    const idOfElem = code + day + time.split("-")[0].split(" ")[0].split(":").join(""); 
+                    if (day !== dayI) {
+                        continue;
+                    }
+                    if (idCurr === idOfElem) {
+                        continue;
+                    }
+                    if ((start > startI) && (start < endI)) {
+                        console.log("Scenario 1");
+                        currentOverlaps.set(idCurr, [...currentOverlaps.get(idCurr) ?? [], idOfElem])
+                        currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], idCurr])
+                    } else if ((end > startI) && (end < endI)) {
+                        console.log("Scenario 2");
+                        currentOverlaps.set(idCurr, [...currentOverlaps.get(idCurr) ?? [], idOfElem])
+                        currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], idCurr])
+                    } else if ((start <= startI) && (end >= endI)) {
+                        console.log("Scenario 3");
+                        currentOverlaps.set(idCurr, [...currentOverlaps.get(idCurr) ?? [], idOfElem])
+                        currentOverlaps.set(idOfElem, [...currentOverlaps.get(idOfElem) ?? [], idCurr])
+                    }
+                }
+                    
+            }
+                
+            console.log("Overlaps: ");
+            console.log(currentOverlaps);
+
+            await chrome.storage.local.set({ overlaps: [...currentOverlaps]});  
+
         }
+
+        //this is the code for when the element is being added to the page
+        //when the element is being removed from the page, need to check all overlaps it is a part of, and remove itself from those
+        //lowkey shouldn't be very difficult
+          
         
    }  
 })();
